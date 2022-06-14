4bfe76d57f7fdf2733bebb2d988de63e541a414b
nombreUsuario <miEmail@domain.com>
2022-06-13 22:59:57 -0300

    cambios en el fondo

--- funciones.js
+++ funciones.js
@@ -70,46 +70,80 @@ var obstaculo=[]
 };
 
 
-function dibujar() {
 
-
-    ctx.drawImage(imamgenFondo, 0, 0)
-    ctx.drawImage(imagenCohete, cx, cy);
-
-    for(var i=0; i<obstaculo.length;i++ ){
-
-        ctx.drawImage(avion747derecha, obstaculo[i].x, obstaculo[i].y);
-        ctx.drawImage(f16derecha, obstaculo[i].x, 70);
-        ctx.drawImage(nubetor, obstaculo[i].x, 180)
-
-        obstaculo[i].x--;
-
-        if (obstaculo[i].x==300){
-            obstaculo.push({
-                x: canvas.width,
-                y: Math.floor(Math.random()*avion747derecha.height)-avion747derecha.height
-            })
+/*
+function fondo(){
+    this.x=0, this.y=0, this.w=imamgenFondo.width, this.h= imamgenFondo.height;
+    this.render= function (){
+        ctx.drawImage(imamgenFondo,0,this.y++);
+        if(this.y<= -600){
+            this.y=0;
         }
     }
 
- //   ctx.drawImage(pasto,0,500)
-   // ctx.drawImage(obstaculoizq),px+constpy;
 
-    if(cy>(canvas.height/2-70)) {
-        cy -= 1;
-    }
-
-    if(derPresionado && cx<canvas.width-cohete.anchoCohete ){
-     cx+=5;
-    }else if (izqpresionado && cx>7 ){
-        cx-=5;
-
-    }
-   // for(var i=0; i<obstaculo.)
-
-    requestAnimationFrame(dibujar);
 }
-dibujar();
+var fondo= new fondo();
+*/
+var gamespeed= 2;
+var fy=0;
+var f2= 600;
 
+function dibujar() {
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(imamgenFondo, 0, fy);
+    ctx.drawImage(imamgenFondo, 0, f2);
+    if (fy < -600) {
+        fy = 600 + f2 - gamespeed;
+    } else(fy-= gamespeed);
+    if (f2 < -600) {
+        f2 = 600 + fy - gamespeed;
+    } else {
+        f2 -= gamespeed;
+    }
+/*
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.save();
+    fondo.render()
+ctx.restore(); */
+        ctx.drawImage(imagenCohete, cx, cy);
+
+
+        for (var i = 0; i < obstaculo.length; i++) {
+
+            ctx.drawImage(avion747derecha, obstaculo[i].x, obstaculo[i].y);
+            ctx.drawImage(f16derecha, obstaculo[i].x, 70);
+            ctx.drawImage(nubetor, obstaculo[i].x, 180)
+
+            obstaculo[i].x--;
+
+            if (obstaculo[i].x == 300) {
+                obstaculo.push({
+                    x: canvas.width,
+                    y: Math.floor(Math.random() * avion747derecha.height) - avion747derecha.height
+                })
+            }
+        }
+
+        //   ctx.drawImage(pasto,0,500)
+        // ctx.drawImage(obstaculoizq),px+constpy;
+
+        if (cy > (canvas.height / 2 - 70)) {
+            cy -= 1;
+        }
+
+        if (derPresionado && cx < canvas.width - cohete.anchoCohete) {
+            cx += 5;
+        } else if (izqpresionado && cx > 7) {
+            cx -= 5;
+
+        }
+        // for(var i=0; i<obstaculo.)
+
+        requestAnimationFrame(dibujar);
+    }
+
+    dibujar();
 
 
